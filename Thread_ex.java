f9cc5b96d44d3a14da4e13686f9930c94ab47561
madhusudhanreddyk <151204022+madhusudhanreddyk@users.noreply.github.com>
2025-05-31 15:59:22 +0530

    Add files via upload

---
+++ Thread_ex.java
@@ -0,0 +1,35 @@
+package javaprograms;
+
+class Car extends Thread{
+	public void run() {
+		for(int i =1;i<=3;i++) {
+			System.out.println("BMW car is moving"+i);
+			try {
+				Thread.sleep(500);
+			}catch(Exception e) {
+			}
+		}
+	}
+}
+class Bike extends Thread{
+	public void run() {
+		for(int i =1;i<=3;i++) {
+			System.out.println("ninja bike is moving"+i);
+			try {
+				Thread.sleep(500);
+			}catch(Exception e) {
+			}
+		}
+	}
+	
+}
+public class Thread_ex {
+
+	public static void main(String[] args) {
+			new Car().start();
+			new Bike().start();
+		
+
+	}
+
+}
